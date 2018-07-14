35fdfef75b197218982ac4045aceae1ef4fda313
macotok <monochromer@r8.ucom.ne.jp>
2018-07-14 22:09:45 +0900

    copyright追加

---
+++ src/js/components/CopyRight.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const CopyRight = () => (
+  <footer>
+    <p className="m-text02">
+      (C) 2018 kasoutsuukachan.com
+    </p>
+  </footer>
+);
+
+export default CopyRight;
--- src/js/container/App.jsx
+++ src/js/container/App.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import Header from '../components/Header';
 import Sns from '../components/Sns';
 import FooterNav from '../components/FooterNav';
+import CopyRight from '../components/CopyRight';
 
 const App = () => (
   <div>
     <Header />
     <Sns />
     <FooterNav />
+    <CopyRight />
   </div>
 );
 
