25799068f9cd5ea0b2146e67859cc023ebfb516c
macotok <monochromer@r8.ucom.ne.jp>
2018-07-14 15:04:03 +0900

    header作成

---
+++ src/js/components/Header.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Header = () => (
+  <div>
+    <header className="m-header01">
+      <div className="m-header01-inner">
+        <p className="m-header01-title">
+          <img src="../images/logo.gif" width="300" alt="仮想通貨ちゃん" />
+        </p>
+        <h1 className="m-header01-subTitle">
+          「仮想通貨ちゃん」は主要通貨はもちろん、今後上がるかもしれないマニアックな草コイン、トークンについて情報交換するサイトだよ
+        </h1>
+      </div>
+    </header>
+  </div>
+);
+
+export default Header;
--- src/js/container/App.jsx
+++ src/js/container/App.jsx
@@ -1,9 +1,8 @@
 import React from 'react';
+import Header from '../components/Header';
 
 const App = () => (
-  <div>
-    Hello World!
-  </div>
+  <Header />
 );
 
 export default App;
