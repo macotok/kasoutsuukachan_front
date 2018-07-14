1c52659f4251d959dd4bb422b2025578155fa977
macotok <monochromer@r8.ucom.ne.jp>
2018-07-14 21:53:45 +0900

    footer navを追加

---
+++ src/js/components/FooterNav.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+const FooterNav = () => (
+  <nav className="m-list02">
+    <ul className="m-list02-listBlock">
+      <li className="m-list02-list">
+        <a href="/" className="m-list02-link">
+          このサイトについて
+        </a>
+      </li>
+      <li className="m-list02-list">
+        <a href="/" className="m-list02-link">
+          利用規約
+        </a>
+      </li>
+      <li className="m-list02-list">
+        <a href="/" className="m-list02-link">
+          お問い合わせ
+        </a>
+      </li>
+    </ul>
+  </nav>
+);
+
+export default FooterNav;
--- src/js/container/App.jsx
+++ src/js/container/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Header from '../components/Header';
 import Sns from '../components/Sns';
+import FooterNav from '../components/FooterNav';
 
 const App = () => (
   <div>
     <Header />
     <Sns />
+    <FooterNav />
   </div>
 );
 
