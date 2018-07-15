4a96029406e177018630ed9095129f4cc44f7b12
macotok <monochromer@r8.ucom.ne.jp>
2018-07-15 17:49:17 +0900

    container/App.jsxをapp.jsxに移動

--- src/js/app.jsx
+++ src/js/app.jsx
@@ -1,6 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './container/App';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Home from './container/Home';
+import About from './container/About';
+import Terms from './container/Terms';
+import Contact from './container/Contact';
+
+const App = () => (
+  <BrowserRouter>
+    <div>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/about" component={About} />
+        <Route path="/terms" component={Terms} />
+        <Route path="/contact" component={Contact} />
+      </Switch>
+    </div>
+  </BrowserRouter>
+);
 
 ReactDOM.render(
   <App />,
