0cde594ea854ca3fe9796b8c54c1c46c27e7efd6
macotok <monochromer@r8.ucom.ne.jp>
2018-07-16 23:07:53 +0900

    ロゴを押下したらtopに遷移

--- src/js/components/Header.jsx
+++ src/js/components/Header.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Header = () => (
   <div>
     <header className="m-header01">
       <div className="m-header01-inner">
         <p className="m-header01-title">
-          <img src="../images/logo.gif" width="300" alt="仮想通貨ちゃん" />
+          <Link to="/">
+            <img src="../images/logo.gif" width="300" alt="仮想通貨ちゃん" />
+          </Link>
         </p>
         <h1 className="m-header01-subTitle">
           「仮想通貨ちゃん」は主要通貨はもちろん、今後上がるかもしれないマニアックな草コイン、トークンについて情報交換するサイトだよ
