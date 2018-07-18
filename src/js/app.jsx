db6937a0f1afb7a97e666fd04277ea21b6624394
macotok <monochromer@r8.ucom.ne.jp>
2018-07-19 02:44:47 +0900

    redux設定中

--- src/js/app.jsx
+++ src/js/app.jsx
@@ -1,10 +1,42 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { combineReducers, createStore } from 'redux';
 import Home from './container/Home';
 import About from './container/About';
 import Terms from './container/Terms';
 import Contact from './container/Contact';
+
+function threadReducer(state = [], action) {
+  return state;
+}
+
+function postReducer(state = '', action) {
+  switch (action.type) {
+    case 'updatePost':
+      return action.payload;
+  }
+
+  return state;
+}
+
+const allReducers = combineReducers({
+  thread: threadReducer,
+  post: postReducer,
+});
+
+const store = createStore(allReducers);
+
+const postAction = {
+  type: 'updatePost',
+  payload: {
+    text: 'first post',
+  },
+};
+
+store.dispatch(postAction);
+
+console.log(store.getState());
 
 const App = () => (
   <BrowserRouter>
