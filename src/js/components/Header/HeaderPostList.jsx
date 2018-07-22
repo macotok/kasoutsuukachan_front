e340fafcbc9e5a38245683cacaaa898f8ce07165
macotok <monochromer@r8.ucom.ne.jp>
2018-07-22 17:18:52 +0900

    一部componentをclassからconstへリファクタリング

--- src/js/components/Header/HeaderPostList.jsx
+++ src/js/components/Header/HeaderPostList.jsx
@@ -4,48 +4,39 @@ import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { rate, priceShape, priceBoolean } from '../../libraries/Utils';
 
-class HeaderPostList extends React.Component {
-  constructor(props) {
-    super(props);
-    this.rate = rate;
-    this.priceShape = priceShape;
-    this.priceBoolean = priceBoolean;
-  }
+const HeaderPostList = (props) => {
+  const { match, ticker } = props;
+  const tickerName = match.params.ticker;
+  const tickerPage = ticker.find(t => (
+    t.ticker === tickerName
+  ));
 
-  render() {
-    const { match, ticker } = this.props;
-    const tickerName = match.params.ticker;
-    const tickerPage = ticker.find(t => (
-      t.ticker === tickerName
-    ));
-
-    return (
-      <header className="m-header02">
-        <div className="m-header02-inner">
-          <div className="m-header02-arrow">
-            <Link to="/" className="m-header02-icon">
-              &#xf3d2;
-            </Link>
-          </div>
-          <div className="m-header02-title">
-            <div className={`m-header02-ticker m-header02-${tickerPage.ticker}`} />
-            <h1>
-              {tickerPage.title}
-            </h1>
-            <div className="m-header02-priceBlock">
-              <p className={`m-header02-lastPrice ${this.priceBoolean(tickerPage.nowPrice, tickerPage.lastPrice)}`}>
-                {this.rate(tickerPage.nowPrice, tickerPage.lastPrice)}
-              </p>
-              <p className="m-header02-price">
-                {this.priceShape(tickerPage.nowPrice)}
-              </p>
-            </div>
+  return (
+    <header className="m-header02">
+      <div className="m-header02-inner">
+        <div className="m-header02-arrow">
+          <Link to="/" className="m-header02-icon">
+            &#xf3d2;
+          </Link>
+        </div>
+        <div className="m-header02-title">
+          <div className={`m-header02-ticker m-header02-${tickerPage.ticker}`} />
+          <h1>
+            {tickerPage.title}
+          </h1>
+          <div className="m-header02-priceBlock">
+            <p className={`m-header02-lastPrice ${priceBoolean(tickerPage.nowPrice, tickerPage.lastPrice)}`}>
+              {rate(tickerPage.nowPrice, tickerPage.lastPrice)}
+            </p>
+            <p className="m-header02-price">
+              {priceShape(tickerPage.nowPrice)}
+            </p>
           </div>
         </div>
-      </header>
-    );
-  }
-}
+      </div>
+    </header>
+  );
+};
 
 HeaderPostList.propTypes = {
   match: PropTypes.shape({
--- src/js/components/ThreadList.jsx
+++ src/js/components/ThreadList.jsx
@@ -4,45 +4,36 @@ import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { rate, priceShape, priceBoolean } from '../libraries/Utils';
 
-class ThreadList extends React.Component {
-  constructor(props) {
-    super(props);
-    this.rate = rate;
-    this.priceShape = priceShape;
-    this.priceBoolean = priceBoolean;
-  }
-
-  render() {
-    const { thread } = this.props;
-    return (
-      <div className="m-threadList01">
-        <h2 className="m-title01">
-          スレッド一覧
-        </h2>
-        <ul className="m-threadList01-block">
-          {thread.map(t => (
-            <li className="m-threadList01-list" key={t.id}>
-              <Link to={`/${t.ticker}`}>
-                <div className={`m-threadList01-icon m-threadList01-${t.ticker}`} />
-                <p className="m-threadList01-text">
-                  {t.title}
+const ThreadList = (props) => {
+  const { thread } = props;
+  return (
+    <div className="m-threadList01">
+      <h2 className="m-title01">
+        スレッド一覧
+      </h2>
+      <ul className="m-threadList01-block">
+        {thread.map(t => (
+          <li className="m-threadList01-list" key={t.id}>
+            <Link to={`/${t.ticker}`}>
+              <div className={`m-threadList01-icon m-threadList01-${t.ticker}`} />
+              <p className="m-threadList01-text">
+                {t.title}
+              </p>
+              <div className="m-threadList01-right">
+                <p className={`m-threadList01-lastPrice ${priceBoolean(t.nowPrice, t.lastPrice)}`}>
+                  {rate(t.nowPrice, t.lastPrice)}
                 </p>
-                <div className="m-threadList01-right">
-                  <p className={`m-threadList01-lastPrice ${this.priceBoolean(t.nowPrice, t.lastPrice)}`}>
-                    {this.rate(t.nowPrice, t.lastPrice)}
-                  </p>
-                  <p className="m-threadList01-price">
-                    {this.priceShape(t.nowPrice)}
-                  </p>
-                </div>
-              </Link>
-            </li>
-          ))}
-        </ul>
-      </div>
-    );
-  }
-}
+                <p className="m-threadList01-price">
+                  {priceShape(t.nowPrice)}
+                </p>
+              </div>
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
 
 ThreadList.propTypes = {
   thread: PropTypes.arrayOf(PropTypes.shape({})),
