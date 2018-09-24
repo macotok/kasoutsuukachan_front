8c1700c57da5fa31f96a8c11228de5e09b393770
macotok <monochromer@r8.ucom.ne.jp>
2018-09-24 12:45:16 +0900

    arrange source

--- src/js/libraries/Utils.jsx
+++ src/js/libraries/Utils.jsx
@@ -1,11 +1,13 @@
+import { NUMBER_DIGITS } from '../defines/Defines';
+
 /**
  * 任意の桁で四捨五入
  * @param num 四捨五入する値
  * @param ten どの桁で四捨五入するか
  * @returns 四捨五入した値
  */
-const floor = (num, ten) => (
-  Math.floor(num * (10 ** ten)) / (10 ** ten)
+const floor = (num, digits) => (
+  Math.floor(num * (10 ** digits)) / (10 ** digits)
 );
 
 const arrangeNotComma = (num) => {
@@ -20,7 +22,7 @@ const arrangeNotComma = (num) => {
 const priceDifferenceCalc = (nowPrice, lastPrice) => {
   const calcDiff = nowPrice - lastPrice;
   const calc = (calcDiff / lastPrice) * 100;
-  return floor(calc, 2);
+  return floor(calc, NUMBER_DIGITS);
 };
 
 // 昨日の値と比較して%で表示
@@ -34,15 +36,12 @@ export const rate = (nowPrice, lastPrice) => {
 // 3桁以上はカンマをつける
 export const priceShape = (nowPrice) => {
   if (!nowPrice) return false;
-  return `${arrangeNotComma(floor(nowPrice, 2))}円`;
+  return `${arrangeNotComma(floor(nowPrice, NUMBER_DIGITS))}円`;
 };
 
 // 昨日の値と比較してプラスかマイナスかでテキストの色を変える
 export const priceBoolean = (nowPrice, lastPrice) => {
   if (!lastPrice) return false;
   const calcResult = priceDifferenceCalc(nowPrice, lastPrice);
-  if (calcResult > 0) {
-    return 'plus';
-  }
-  return 'minus';
+  return calcResult > 0 ? 'plus' : 'minus';
 };
